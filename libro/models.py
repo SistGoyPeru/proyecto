e793d8137cfb0ba6f69f18f7ed15471509fd4f0a
SistGoy <alexgoyzueta2018@gmail.com>
2021-04-15 20:06:27 -0500

    migration ok

--- libro/models.py
+++ libro/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+class Autor(models.Model):
+    id=models.AutoField(primary_key=True)
+    nombre=models.CharField(max_length=50,blank = False,null=False)
+    apellidos=models.CharField(max_length=100,blank = False,null=False)
+    nacionalidad=models.CharField(max_length=50,blank = False,null=False)
+    descripcion=models.TextField(blank = False,null=False)
