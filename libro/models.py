5d5a7ec1f028ec480d4eec25f98286028e285559
SistGoy <alexgoyzueta2018@gmail.com>
2021-04-15 22:45:37 -0500

    modelos ok

--- libro/models.py
+++ libro/models.py
@@ -6,6 +6,7 @@ class Autor(models.Model):
     apellidos=models.CharField(max_length=100,blank = False,null=False)
     nacionalidad=models.CharField(max_length=50,blank = False,null=False)
     descripcion=models.TextField(blank = True ,null=True)
+    fecha_creacion=models.DateField('Fecha de creacion', auto_now=True, auto_now_add=False)
 
     class Meta:
         verbose_name='Autor'
@@ -14,3 +15,17 @@ class Autor(models.Model):
     
     def __str__(self):
         return self.nombre
+
+class Libro(models.Model):
+    id=models.AutoField(primary_key=True)
+    titulo = models.CharField('Titulo', max_length=50,blank = False,null=False)
+    fecha_publicacion  = models.DateField('Fecha de publicacion', blank = False,null=False)
+    autor_id = models.ManyToManyField(Autor)
+    fecha_creacion=models.DateField('Fecha de creacion', auto_now=True, auto_now_add=False)
+
+    class Meta:
+        verbose_name='Libro'
+        verbose_name_plural='Libros'
+        ordering=['titulo']
+    def __str__(self):
+        return self.titulo
