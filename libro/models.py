a4f574c8b6ae45fb01a43976ac82e74239fa6c3f
SistGoy <alexgoyzueta2018@gmail.com>
2021-04-15 21:49:49 -0500

    7 ok

--- libro/models.py
+++ libro/models.py
@@ -5,4 +5,12 @@ class Autor(models.Model):
     nombre=models.CharField(max_length=50,blank = False,null=False)
     apellidos=models.CharField(max_length=100,blank = False,null=False)
     nacionalidad=models.CharField(max_length=50,blank = False,null=False)
-    descripcion=models.TextField(blank = False,null=False)
+    descripcion=models.TextField(blank = True ,null=True)
+
+    class Meta:
+        verbose_name='Autor'
+        verbose_name_plural='Autores'
+        ordering=['nombre']
+    
+    def __str__(self):
+        return self.nombre
