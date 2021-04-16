b3aeb9665ed421332713b555ed024e7fd345b4d5
SistGoy <alexgoyzueta2018@gmail.com>
2021-04-15 21:30:58 -0500

    rgistra autor

--- biblioteca/settings.py
+++ biblioteca/settings.py
@@ -76,8 +76,13 @@ WSGI_APPLICATION = 'biblioteca.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'biblioteca',
+        'USER': 'postgres',
+        'PASSWORD': '2310',
+        'HOST': 'localhost',
+        'DATABASE': '5432',
+
     }
 }
 
