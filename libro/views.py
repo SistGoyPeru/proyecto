4027c9224a632ad2efb8484013ce0d89108b4a3d
SistGoy <alexgoyzueta2018@gmail.com>
2021-04-16 09:39:13 -0500

    get post

--- libro/views.py
+++ libro/views.py
@@ -1,4 +1,17 @@
-from django.shortcuts import render
+from libro.forms import AutorForm
+from django.shortcuts import redirect, render
+from .forms import AutorForm
 
 def Home(request):
     return render(request,'index.html')
+
+def crearAutor(request):
+    if request.method=='POST':
+        autor_form=AutorForm(request.POST)
+        if autor_form.is_valid():
+            autor_form.save()
+            return redirect('index')
+
+    else:
+        autor_form=AutorForm()
+    return render(request,'libro/crear_autor.html',{'autor_form':autor_form})            
